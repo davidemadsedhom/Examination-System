a7d5c9d374adf516e02ebbfdd82ef282c1993036
davidemadsedhom <davidemadsedhom@gmail.com>
2026-09-19 22:07:23 +0300

    Exam class

---
+++ Examination System/Exam.cs
@@ -0,0 +1,27 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    namespace Examination_System
+    {
+      
+         public abstract class Exam
+        {
+            public int Time { get; set; }            
+            public int NumberOfQuestions { get; set; }
+
+           
+            public List<Question> Questions { get; set; }
+
+            public Exam(int time, int numberOfQuestions)
+            {
+                Time = time;
+                NumberOfQuestions = numberOfQuestions;
+                Questions = new List<Question>();
+            }
+
+            
+            public abstract void ShowExam();
+        }
+    }
+   
