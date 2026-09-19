26449cfaaa480e1c6bbeb25a880add89ae34a34c
davidemadsedhom <davidemadsedhom@gmail.com>
2026-09-19 21:50:15 +0300

    implement a Qustion class

--- Examination System/Question.cs
+++ Examination System/Question.cs
@@ -4,8 +4,34 @@ using System.Text;
 
 namespace Examination_System
 {
-    public abstract class  Question
+    public abstract class Question : ICloneable, IComparable
     {
 
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+
+        public Question(string header, string body, int mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+        }
+
+
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+
+        public abstract void ShowQuestion();
+
+        public int CompareTo(object obj)
+        {
+            Question q = (Question)obj;
+            return this.Mark.CompareTo(q.Mark);
+        }
     }
 }
