ab92aab362693d758f5dcaa1de95470f4f37f19c
davidemadsedhom <davidemadsedhom@gmail.com>
2026-09-19 21:21:10 +0300

    Add project files.

---
+++ Examination System/Answer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examination_System
+{
+    internal class Answer
+    {
+        int answerId;
+        string answerText;
+    }
+}
