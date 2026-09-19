749b13383aa3027a9590dbfd83938af4e122f443
davidemadsedhom <davidemadsedhom@gmail.com>
2026-09-19 22:29:29 +0300

    done the Exame

--- Examination System/Program.cs
+++ Examination System/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Examination_System
+﻿using Examination_System.ExamTypes;
+using Examination_System.QuestionTypes;
+
+namespace Examination_System
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            Subject math = new Subject(1, "Math");
+
+            
+            math.SubjectExam = new FinalExam(60, 1); 
+
+            
+            Question q1 = new TrueFalse("Q1", "Is OOP Easy?", 10);
+            q1.RightAnswer = q1.AnswerList[0]; 
+            math.SubjectExam.Questions.Add(q1);
+
+            
+            math.SubjectExam.ShowExam();
         }
     }
 }
