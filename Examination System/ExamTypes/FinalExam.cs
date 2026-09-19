6ca3f782d14894fac21ee6d73c02ce64e814a1fb
davidemadsedhom <davidemadsedhom@gmail.com>
2026-09-19 22:15:42 +0300

    COMMIT ALL IN EXAM BRANSH AND CREATE A EXAME TYPE

---
+++ Examination System/ExamTypes/FinalExam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examination_System.ExamTypes
+{
+    internal class FinalExam : Exam
+    {
+
+        public FinalExam(int time, int num) : base(time, num) { }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine("=== Final Exam ===");
+            Console.WriteLine($"Time: {Time} Minutes | Questions: {NumberOfQuestions}");
+            Console.WriteLine("-------------------------");
+
+            foreach (var q in Questions)
+            {
+                q.ShowQuestion(); 
+                Console.WriteLine("-------------------------");
+            }
+
+            
+        }
+    }
+}
---
+++ Examination System/ExamTypes/PracticalExam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examination_System.ExamTypes
+{
+    public class PracticalExam : Exam
+    {
+        public PracticalExam(int time, int num) : base(time, num) { }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine("=== Practical Exam ===");
+            Console.WriteLine($"Time: {Time} Minutes | Questions: {NumberOfQuestions}");
+            Console.WriteLine("-------------------------");
+
+            foreach (var q in Questions)
+            {
+                q.ShowQuestion();
+
+                Console.WriteLine($"-> Right Answer: {q.RightAnswer}");
+                Console.WriteLine("-------------------------");
+            }
+        }
+    }
+}
